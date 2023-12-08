df7d1492783f794d650ff9f5292a12b5bae392ba
Vladimir <pestov.web@yandex.ru>
2023-12-08 19:10:11 +1000

    feat: blank pages & components

---
+++ components/shared/PwButton.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: shared/PwButton
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/shared/PwLink.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: shared/PwLink
+  </div>
+</template>
+
+<style scoped></style>
